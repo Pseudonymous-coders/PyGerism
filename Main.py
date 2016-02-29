0b3ab8bc5354c1eeb1f4b83b206a6467d2ef01b6
Eli Smith <Eli Smith>
2016-02-29 16:09:50 -0600

    Add all files

---
+++ Main.py
@@ -0,0 +1,72 @@
+from threading import Thread
+from time import sleep
+from GUI import ProgressBar, MessageDialog, FileDialog
+from pytexter import Docxer
+from copyScape import makeReport
+from math import factorial
+
+def exit_app(na):
+    gui.destroy()
+    message.destroy()
+    try:
+        use = str(na)[20]
+    except IndexError:
+        use = str(na)
+    print "Usable callback: %s" % use
+    exit(0)
+
+
+gui = ProgressBar(800, 100, exit_app)
+dialog = FileDialog(gui.get_parent())
+message = MessageDialog(gui.get_parent(), "ERROR", "Couldn't load the requested folder")
+
+doc = Docxer()
+path = None
+
+
+def slow_move(move):
+    for a in range(move):
+        gui.add_percent(1)
+        sleep(0.8)
+
+
+def on_start_app():
+    global path
+    print "Analyzer Started\n\n"
+    gui.set_percent(0)
+    gui.set_text("Reading and Converting the files")
+    print "Getting files..."
+    files = doc.get_folder(path)
+    gui.set_percent(2)
+    gui.set_text("Reading and Converting the files")
+    gui.set_prog_text(len(files))
+    gui.set_percent(6)
+    gui.set_text("Reading and Converting the files")
+    print "Reading and converting..."
+    doc.run_files(path, files, path + "/Examined/")
+    print "Examined/Converted"
+    gui.set_percent(30)
+    gui.set_text("DONE reading and converting")
+    sleep(2)
+    gui.set_percent(35)
+    gui.set_text("Analyzing essays")
+    Thread(target=slow_move, args=(60,)).start()
+    factorial(len(files))
+    makeReport.getReport(path + "/Examined/")
+    gui.set_percent(100)
+    gui.set_text("Done!!!")
+    gui.set_cancel_text("DONE")
+    print "Finished all of the examinations"
+
+
+if __name__ == "__main__":
+    dialog_response, path = dialog.get_folder()
+    if dialog_response:
+        exit_app(0)
+    if path is None:
+        message.connect(exit_app)
+        message.show()
+        exit_app(0)
+    path += "/"
+    exit_app(0) if path is None else path
+    gui.start(on_start_app)
---
+++ pytexter.py
@@ -0,0 +1,113 @@
+# coding=utf-8
+from time import sleep
+from docx2txt import process
+from os import listdir, mkdir
+from shutil import rmtree
+from string import printable
+from os.path import isfile, join, splitext, exists
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
+from pdfminer.pdfparser import PDFSyntaxError
+from FilesConverter import odt_get_text
+from Configuration_Files import config
+
+class Docxer:
+    def __init__(self):
+        self.name = ""
+
+    @staticmethod
+    def get_folder(path):
+        try:
+            files = [f for f in listdir(path) if isfile(join(path, f))]
+            print "Found (%d) Files: %s" % (len(files), str(files))
+            return files
+        except (WindowsError, Exception):
+            return [""]
+
+    def run_files(self, dirs, ready_files, output=""):
+        sleep(1)
+        if exists(output):
+            try:
+                rmtree(output)
+            except (WindowsError, Exception):
+                print "Unable to access folder"
+                exit(0)
+        sleep(1)
+        try:
+            mkdir(output)
+        except (WindowsError, Exception):
+            sleep(1)
+            mkdir(output)
+        for file in ready_files:
+            extension = splitext(dirs + file)[1]
+            print "\r\rConverting: " + file
+            if "docx" in extension and config.run_docx:
+                try:
+                    to_write = str(self.process_doxc(dirs + file).encode('ascii', 'ignore'))
+                except (IOError, Exception):
+                    to_write = "N?A"
+                    print "Couldn't read DOCX: " + file
+            elif "pdf" in extension and config.run_pdf:
+                try:
+                    to_write = str(self.process_pdf(dirs + file))
+                except (PDFSyntaxError, Exception):
+                    to_write = "N?A"
+                    print "Couldn't read PDF: " + file
+            elif "odt" in extension and config.run_odt:
+                try:
+                    to_write = str(odt_get_text(dirs + file).encode('ascii', 'ignore'))
+                except (IOError, Exception):
+                    to_write = "N?A"
+                    print "Couldn't read ODT: " + file
+            else:
+                try:
+                    tofile = open(dirs + file)
+                    to_write = str(tofile.read())
+                    tofile.close()
+                except IOError:
+                    print "Couldn't read file: " + file
+                    to_write = "N?A"
+            # Only print printable
+            sleep(0.1)
+            try:
+                printa = set(printable)
+                to_write = to_write.replace(" ", "(<<&=SPACE=&>>)").replace("", "")
+                to_write = filter(lambda x: x in printa, to_write).replace("\t", "")
+                to_write = to_write.replace("(<<&=SPACE=&>>)", " ").strip()
+                new_file = open(str(output + (file[:file.rfind(".")]) + ".txt"), 'w')
+                new_file.write(to_write)
+                new_file.close()
+            except (WindowsError, Exception):
+                print "Error writing the file"
+        sleep(1)
+        print "Done..."
+
+    @staticmethod
+    def process_pdf(path):
+        print "Processing pdf"
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        fp = file(path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos = set()
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
+                                      check_extractable=True):
+            interpreter.process_page(page)
+        text = retstr.getvalue()
+        fp.close()
+        device.close()
+        retstr.close()
+        return text
+
+    def process_doxc(self, files):
+        self.name = file
+        return process(str(files))
