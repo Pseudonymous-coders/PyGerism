f5afa9f6822dd8361b907776872ed87debda80ac
Eli Smith <plunkinguitar@gmail.com>
2016-10-20 14:36:23 -0500

    Inital work on text analysis

--- processing/main.py
+++ processing/main.py
@@ -1,4 +1,34 @@
 import glob
 
-def getFiles():
-    files = glob.glob("../")
+def getFiles(path):
+    files = []
+    names = []
+    output = []
+    rawFiles = glob.glob(path+"*.txt")
+    for thing in rawFiles:
+        f = open(thing, "r").read()
+        files.append(f)
+        if thing.rfind('\\') != -1:
+            name = thing[int(thing.rfind('\\'))+1:]
+        elif thing.rfind('/'):
+            name = thing[int(thing.rfind('/'))+1:]
+        else:
+            name = thing
+        names.append(name)
+    output.append(names)
+    output.append(files)
+    return output
+
+raw = getFiles("../essays/Examined/")
+
+rawNames = raw[0]
+rawEssays = raw[1]
+
+essays = []
+
+for i in range(len(rawEssays)):
+    essayInfo = {}
+    essayInfo['name'] = rawNames[i]
+    essayInfo['essay'] = rawEssays[i]
+    essays.append(essayInfo)
+print essays
